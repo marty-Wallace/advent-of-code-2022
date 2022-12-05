2772b05f57bdc4b86f50944005d27bb5d080678e
marty <martyv.wallace@gmail.com>
2022-12-04 22:39:46 -0800

    fixup day 5 a bit

--- days/day5.py
+++ days/day5.py
@@ -4,9 +4,8 @@ from days.day import Day
 
 class Day5(Day):
 
-    def part_one(self):
-        lines = []
-        instructions = []
+    def parse_input(self):
+        lines, instructions = [], []
         ll = 0
         for line in self.input_stream:
             if line.strip().startswith('['):
@@ -19,20 +18,21 @@ class Day5(Day):
                 instructions.append(line.strip())
 
         stacks = []
-        for i in range(ll+1):
+        for _ in range(ll + 1):
             stacks.append([])
         lines.reverse()
         for line in lines:
-            for i in range(ll+1):
-                p = i*4 + 1
+            for i in range(ll + 1):
+                p = i * 4 + 1
                 if p < len(line) and line[p] != ' ':
                     stacks[i].append(line[p])
 
+        return lines, instructions, stacks
+
+    def part_one(self):
+        lines, instructions, stacks = self.parse_input()
         for move in instructions:
-            _, c, _, s, _, d = move.split(' ')
-            c = int(c)
-            s = int(s)
-            d = int(d)
+            c, s, d = map(int, move.split(' ')[1::2])
             for _ in range(c):
                 stacks[d-1].append(stacks[s-1].pop())
 
@@ -40,34 +40,9 @@ class Day5(Day):
         self.p(res)
 
     def part_two(self):
-        lines = []
-        instructions = []
-        ll = 0
-        for line in self.input_stream:
-            if line.strip().startswith('['):
-                lines.append(line.strip('\n'))
-            elif line.strip().startswith('1'):
-                ll = int(line.strip().split(' ')[-1]) - 1
-            elif line.strip() == '':
-                pass
-            else:
-                instructions.append(line.strip())
-
-        stacks = []
-        for i in range(ll+1):
-            stacks.append([])
-        lines.reverse()
-        for line in lines:
-            for i in range(ll+1):
-                p = i*4 + 1
-                if p < len(line) and line[p] != ' ':
-                    stacks[i].append(line[p])
-
+        lines, instructions, stacks = self.parse_input()
         for move in instructions:
-            _, c, _, s, _, d = move.split(' ')
-            c = int(c)
-            s = int(s)
-            d = int(d)
+            c, s, d = map(int, move.split(' ')[1::2])
             temp = []
             for _ in range(c):
                 temp.append(stacks[s-1].pop())
